855e4566c68473915e0126108dc78640a523679f
Anil Sawant <anil.sawant1@wipro.com>
2016-10-06 16:37:58 +0530

    added showNotification(), msg banner update

--- msgBanners/msg-banners.js
+++ msgBanners/msg-banners.js
@@ -1,38 +1,78 @@
-
-/* ************** Messages scriptlet *************** */
+/******** Message Banners at the bottom ********/
 let showMsgBanner = function (msg, type) {
-	let $msgContainer = $('<div class="msg-container" style="display:none;"> \
-							            <span class="close-msg">&times;</span> \
-							            <span class="msg">This is a msg</span> \
-							          </div>');
-	$(document.body).append($msgContainer);
-	$msgContainer.on('click', function (evt) {
+	let msgContainer = document.createElement('div');
+	msgContainer.className = 'msg-container';;
+	msgContainer.innerHTML = '<span class="close-msg">&times;</span><span class="msg">' + msg + '</span>';
+	document.body.appendChild(msgContainer);
+	let timeoutHandle = setTimeout(function () {
+		document.body.removeChild(msgContainer)
+	}, 5000);
+	msgContainer.addEventListener('click', function (evt) {
 		if (evt.target.className.includes('close-msg')) {
-			$msgContainer.remove();
+			clearTimeout(timeoutHandle);
+			document.body.removeChild(msgContainer)
 		}
 	});
-  $msgContainer.find('.msg').text(msg);
   switch (type.toUpperCase()) {
     case "SUCCESS":
-      $msgContainer.removeClass('error warning').addClass('success').fadeIn(function () {
-        setTimeout(function () {
-          $msgContainer.remove();
-        }, 5000);
-      });
+      msgContainer.className += ' success';
       break;
     case "ERROR":
-      $msgContainer.removeClass('success warning').addClass('error').fadeIn(function () {
-        setTimeout(function () {
-          $msgContainer.remove();
-        }, 10000);
-      });
+			msgContainer.className += ' error';
       break;
     case "WARNING":
-      $msgContainer.removeClass('error success').addClass('warning').fadeIn(function () {
-        setTimeout(function () {
-          $msgContainer.remove();
-        }, 10000);
-      });
+			msgContainer.className += ' warning';
       break;
   }
+	setTimeout(function () {
+		msgContainer.style.opacity = 1;
+	}, 10);
 };
+
+
+/******** Win10 style Notifications ********/
+window.showNotification = function (msg) {
+  window.showNotification.msgQueue.push(msg);
+  if (window.showNotification.msgQueue.length == 1) {
+    window.showNotification.show(window.showNotification.msgQueue[0]);
+  }
+};
+window.showNotification.msgQueue = [];
+window.showNotification.show = function (msg) {
+  let notfContainer = document.createElement('div');
+  notfContainer.style.backgroundColor =  '#555';notfContainer.style.bottom =  '100px';notfContainer.style.border =  '1px solid #555';
+  notfContainer.style.borderRight =  'none';notfContainer.style.boxShadow =  '0 0 5px #555, inset 1px 1px 0 #777, inset -1px 0 0 #777';
+  notfContainer.style.cursor =  'pointer';notfContainer.style.padding =  '5px 10px 5px 5px';notfContainer.style.position =  'fixed';
+  notfContainer.style.right =  '-220px';notfContainer.style.transition = 'right 0.5s cubic-bezier(0, 0.73, 0.24, 1.2)';
+  notfContainer.style.width =  '200px';notfContainer.style.zIndex =  100;
+  let notificationSound = document.createElement('audio');
+  notificationSound.style.display = 'none';notificationSound.setAttribute('autoplay', true);notificationSound.src = 'notify.wav';
+  notfContainer.appendChild(notificationSound);
+  let notfBody = document.createElement('div');
+  notfBody.style.border = '1px solid #666';notfBody.style.boxShadow = 'inset 0 0 3px #666';notfBody.style.display = 'flex';
+  notfBody.style.minHeight = '60px';notfBody.style.padding = '5px';notfBody.style.width = '200px';
+  let notification = document.createElement('div');
+  notification.style.backgroundColor = '#575757';notification.style.color = '#f2f2f2';notification.style.fontFamily = 'monospace';
+  notification.style.margin = 'auto';notification.style.padding = '3px';notification.style.textAlign = 'center';
+  notification.innerText = msg;
+
+  notfBody.appendChild(notification);
+  notfContainer.appendChild(notfBody)
+  document.body.appendChild(notfContainer);
+  setTimeout(function () {
+    notfContainer.style.right = '-10px';
+  }, 10);//show the notification
+  setTimeout(function () {
+    notfContainer.style.transition =  'right .5s ease';
+    notfContainer.style.right = '-220px';
+    setTimeout(function () {
+      document.body.removeChild(notfContainer);
+      if (window.showNotification.msgQueue.length) {
+        window.showNotification.msgQueue.splice(0,1);
+        if (window.showNotification.msgQueue.length) {
+          window.showNotification.show(window.showNotification.msgQueue[0]);
+        }
+      };
+    }, 500);//remove after the transition completes
+  }, 5000);//hide after 5s
+}
